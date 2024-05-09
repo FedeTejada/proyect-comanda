a27b2ecc9d72547ec23166ce9a86bc5e6652dadf
FedeTejada <tejafede@gmail.com>
2024-05-09 13:50:51 -0300

    Nuevo modelo de proyecto

---
+++ main.py
@@ -0,0 +1,8 @@
+from service import menu
+## A desarrollar:
+        ##Mejorar el menu o que no sea ambiguo, buscarlo en service y completarlo.
+def main():
+    menu()
+
+# print(lomito._name)
+main()
