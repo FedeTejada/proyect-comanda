2b193218155ba99d046541b12a943bebaa361eab
ginomoricheti <115089986+ginomoricheti@users.noreply.github.com>
2024-05-18 10:07:25 -0300

    Finished the take product script and addings some comments and documentation

--- main.py
+++ main.py
@@ -9,50 +9,58 @@ from src.service.change_products import frame_modif_product
 from src.service.balance import frame_balance
 from src.service.config import frame_config
 
-# Function for change the main content
+# ------------------------------------------------------------------------------
+# FUNCTION TO CHANGE THE MAIN CONTENT
+
 def change_section(frame, seccion_funcion):
+    """
+    Clear the current frame and load a new section.
+    """
     for widget in frame.winfo_children():
         widget.destroy()
     seccion_funcion(frame)
 
-# Configuration of the window
+# ------------------------------------------------------------------------------
+# CONFIGURATION OF THE WINDOW
+
 root = customtkinter.CTk()
 root.title("Comandero")
-customtkinter.set_appearance_mode("system")  # default
+customtkinter.set_appearance_mode("system")  # Set the appearance mode to system default
 
 # Window dimensions
 width = 1200
 height = 600
 
 # Calculating the x and y position for centering the window
-pos_x = int(root.winfo_screenwidth()/2 - width/2)
-pos_y = int(root.winfo_screenheight()/2 - height/2)
+pos_x = int(root.winfo_screenwidth() / 2 - width / 2)
+pos_y = int(root.winfo_screenheight() / 2 - height / 2)
 
-# Stablish the size and position
+# Set the size and position of the window
 root.geometry(f"{width}x{height}+{pos_x}+{pos_y}")
 
 # Set minimum window size
-root.minsize(1200, 600)  # Example minimum width: 600px, minimum height: 400px
+root.minsize(1200, 600)  # Minimum width: 1200px, minimum height: 600px
 
-# Nav bar configuration
+# ------------------------------------------------------------------------------
+# NAVIGATION BAR CONFIGURATION
+
 frame_nav = CTkFrame(root, 
-                    width=220,
-                    corner_radius=0,
-                    fg_color="#222",
-                    border_color="#0F0F0F",
-                    border_width=1
-                    )
+                     width=220,
+                     corner_radius=0,
+                     fg_color="#222",
+                     border_color="#0F0F0F",
+                     border_width=1)
 frame_nav.pack(side="left", fill="y")
 frame_nav.propagate(False)
 
 frame_content = CTkFrame(root, corner_radius=0)
 frame_content.pack(side="right", expand=True, fill="both")
 
-# Nav Bar Buttons configuration
+# ------------------------------------------------------------------------------
+# NAV BAR BUTTONS CONFIGURATION
 
 # Home Button
 home_button_img_pil = Image.open(".\media\images\home_icon.png")
-
 home_button_img = CTkImage(home_button_img_pil)
 
 home_button = CTkButton(
@@ -60,27 +68,26 @@ home_button = CTkButton(
                 text="",
                 image=home_button_img,
                 compound="left",
-                command=lambda:change_section(frame_content, frame_home),
+                command=lambda: change_section(frame_content, frame_home),
                 width=160,
-                height=40,
-                )
+                height=40)
 home_button.pack(pady=(40, 10), padx=10, anchor="center")
 
+# List of buttons and their corresponding functions
 buttons = [
     ("Tomar Pedidos", frame_orders),
     ("Modificar Productos", frame_modif_product),
     ("Ver Balance", frame_balance),
 ]
 
+# Create and pack each button in the navigation bar
 for text, function in buttons:
     btn = CTkButton(
         frame_nav,
         text=text,
-        command=lambda f=frame_content,
-        fn=function: change_section(f, fn),
+        command=lambda f=frame_content, fn=function: change_section(f, fn),
         width=160,
-        height=40
-        )
+        height=40)
     btn.pack(pady=10, padx=10, anchor="center")
 
 # Configuration Button
@@ -89,8 +96,10 @@ config_button = CTkButton(
                     text="Configuracion",
                     command=lambda: change_section(frame_content, frame_config),
                     fg_color="#808080",
-                    hover_color="#676767"
-                    )
+                    hover_color="#676767")
 config_button.pack(side="bottom", pady=(10, 40), padx=10, anchor="center")
+
+# ------------------------------------------------------------------------------
+# START THE MAIN LOOP
 
 root.mainloop()
--- src/service/orders.py
+++ src/service/orders.py
@@ -1,64 +1,121 @@
+# orders.py
 import tkinter as tk
-from tkinter import messagebox, simpledialog
+from tkinter import messagebox
 from customtkinter import CTkFrame, CTkButton, CTkInputDialog, CTkScrollableFrame
+from src.models.Product import Product
 
-product_buttons = {}  # Dictionary to store product buttons and their quantities
+# ------------------------------------------------------------------------------
+# ARRAY FOR TESTING
+# Initialize some sample products and add them to a test menu array
+
+lomito = Product(33, "Lomito", 100, "Sanguche")
+pizza = Product(11, "Pizza", 500, "Pizza")
+vianda_1 = Product(21, "Vianda 1", 400, "Vianda")
+vianda_2 = Product(22, "Vianda 2", 400, "Vianda")
+hamburguesa = Product(44, "Hamburguesa", 300, "Sanguche")
+empanada = Product(55, "Empanada", 200, "Empanada")
+
+menu = [lomito, pizza, vianda_1, vianda_2, hamburguesa, empanada]
+
+# ------------------------------------------------------------------------------
+# Variables
+# Define dictionaries to store product buttons and selected products
+
+product_buttons = {}  # Dictionary to store product buttons
 selected_products = {}  # Dictionary to store selected products and their quantities
 
+# ------------------------------------------------------------------------------
+# Functions
+
 def add_to_cart(product):
+    """
+    Add a product to the cart or update its quantity.
+    """
     global selected_products
-    if product in selected_products:
-        quantity_dialog = CTkInputDialog(title="Quantity", text=f"Enter quantity for {product}:", initialvalue=selected_products[product])
-        quantity = quantity_dialog.get_input()  # Use get_input attribute to retrieve the input value
-    else:
-        quantity_dialog = CTkInputDialog(title="Quantity", text=f"Enter quantity for {product}:")
-        quantity = quantity_dialog.get_input()
-    
+
+    # Prompt for quantity
+    quantity_dialog = CTkInputDialog(title="Quantity",
+                                     text=f"Enter quantity for {product._name}:")
+    quantity = quantity_dialog.get_input()
+
     if quantity is not None:
         try:
             quantity = int(quantity)  # Convert quantity to integer
-            selected_products[product] = quantity
+            selected_products[product._name] = quantity
         except ValueError:
             messagebox.showerror("Error", "Please enter a valid integer for quantity.")
+
     update_cart_display()
 
-# Update remove_from_cart to delete all items from the cart
+
+def update_cart_display():
+    """
+    Update the cart display with selected products and calculate the total price.
+    """
+    global selected_products
+    cart_listbox.delete(0, tk.END)
+    total_price = 0
+    for product_name, quantity in selected_products.items():
+        cart_listbox.insert(tk.END, f"{product_name} x {quantity}")
+        # Find the product in the menu to get its price
+        product = next((p for p in menu if p._name == product_name), None)
+        if product:
+            total_price += product._price * quantity
+    total_label.config(text=f"Total: ${total_price}")
+
+
+
 def remove_from_cart():
+    """
+    Remove all items from the cart.
+    """
     global selected_products
     if messagebox.askokcancel("Remove All", "Do you want to remove all items from the cart?"):
         selected_products = {}
         update_cart_display()
 
-def update_cart_display():
+def remove_item(event):
+    """
+    Remove a single item from the cart.
+    """
     global selected_products
-    cart_listbox.delete(0, tk.END)
-    for product, quantity in selected_products.items():
-        cart_listbox.insert(tk.END, f"{product} x {quantity}")
-    total_price = sum(quantity * 10 for quantity in selected_products.values())
-    total_label.config(text=f"Total: ${total_price}")
+    index = cart_listbox.curselection()
+    if index:
+        item = cart_listbox.get(index)
+        product_name, _ = item.split(" x ")
+        if messagebox.askokcancel("Remove Item", f"Do you want to remove {product_name} from the cart?"):
+            del selected_products[product_name]
+            update_cart_display()
 
 def finalize_purchase():
-    # Show a dialog box to confirm the purchase
+    """
+    Finalize the purchase and show a confirmation dialog.
+    """
     confirm = messagebox.askquestion("Confirm", "Do you want to finalize the purchase?")
     if confirm == "yes":
         # Logic to finalize the purchase
         pass
 
-def show_products(products_frame, product_list):
-    global product_buttons  # Declare product_buttons as global
+def show_products(products_frame, menu):
+    """
+    Display the product buttons in the products frame.
+    """
+    global product_buttons
     num_columns = 5  # Number of columns before jumping to the next row
     height_button = 80
     pad_y = 8
-    side_text = 'center'
-    if len(product_list) > 30:
-        num_columns = 1  # Change to single column layout if more than 30 products
+    
+    if len(menu) > 30:
+        # Change to single column layout if more than 30 products
+        num_columns = 1
         height_button = 25
         pad_y = 2
-    for i, product in enumerate(product_list):
+    
+    for i, product in enumerate(menu):
         row_num = i // num_columns
         col_num = i % num_columns
         product_button = CTkButton(products_frame,
-                                   text=product,
+                                   text=product._name,
                                    command=lambda p=product: add_to_cart(p),
                                    width=80,
                                    height=height_button,
@@ -69,50 +126,76 @@ def show_products(products_frame, product_list):
                                    border_width=2,
                                    fg_color="#d9d9d9")
         product_button.grid(row=row_num, column=col_num, padx=8, pady=pad_y, sticky="nsew")
-        product_buttons[product] = product_button  # Add button to the dictionary
+        product_buttons[product._name] = product_button  # Use product._name as the key
         products_frame.grid_columnconfigure(col_num, weight=1)  # Columns should expand
         products_frame.grid_rowconfigure(row_num, weight=1)  # Rows should expand
 
-
 def on_resize(event):
-    global product_buttons  # Declare product_buttons as global
-    # Adjust the size of product buttons based on the window width and maintain square aspect ratio
+    """
+    Adjust the size of product buttons based on the window width and maintain square aspect ratio.
+    """
+    global product_buttons
     new_width = max(80, (event.width - 16) // 5)  # Considering horizontal padding
     new_height = new_width  # Maintain square aspect ratio
     for button in product_buttons.values():
         button.config(width=new_width, height=new_height)
 
 def frame_orders(frame):
+    """
+    Set up the orders frame layout and initialize the products and cart sections.
+    """
     frame.grid_columnconfigure(0, weight=2)
     frame.grid_columnconfigure(1, weight=1)
     frame.grid_rowconfigure(0, weight=1)
     frame.configure(fg_color="#f2f2f2")
 
-    products_frame = CTkScrollableFrame(frame, corner_radius=0, fg_color="#f2f2f2")
+    products_frame = CTkScrollableFrame(frame,
+                                        corner_radius=0,
+                                        fg_color="#f2f2f2")
     products_frame.grid(row=0, column=0, sticky="nsew")
 
-    product_list = ["Product 1", "Product 2", "Product 3"] * 13
-    show_products(products_frame, product_list)
+    show_products(products_frame, menu)
 
     global cart_listbox, total_label
-    cart_frame = CTkFrame(frame, corner_radius=0, fg_color="#fff", width=200)
+    cart_frame = CTkFrame(frame,
+                          corner_radius=0,
+                          fg_color="#fff",
+                          width=200)
     cart_frame.grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
 
-    cart_listbox = tk.Listbox(cart_frame, font=("Helvetica", 12), borderwidth=0)
+    cart_listbox = tk.Listbox(cart_frame,
+                              font=("Helvetica", 12),
+                              borderwidth=0)
     cart_listbox.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
-    button_frame = CTkFrame(cart_frame, corner_radius=0, fg_color="#f2f2f2")
+    button_frame = CTkFrame(cart_frame,
+                            corner_radius=0,
+                            fg_color="#f2f2f2")
     button_frame.pack(side="bottom", fill="x")
 
     # Move the total_label before the buttons
-    total_label = tk.Label(button_frame, text="Total: $0", font=("Helvetica", 12))
+    total_label = tk.Label(button_frame,
+                           text="Total: $0",
+                           font=("Helvetica", 12))
     total_label.pack(side="top", padx=2, pady=5)
 
     # Load and display the remove button
-    remove_button = CTkButton(button_frame, text="Clear Cart", command=remove_from_cart, fg_color="#FF0000", hover_color="#FF3333", height=40, width=10)
+    remove_button = CTkButton(button_frame,
+                              text="Clear Cart",
+                              command=remove_from_cart,
+                              fg_color="#FF0000",
+                              hover_color="#FF3333",
+                              height=40,
+                              width=10)
     remove_button.pack(side="left", padx=(0, 5), pady=10)
 
-    finish_button = CTkButton(button_frame, text="Complete Purchase", command=finalize_purchase, fg_color="#008000", hover_color="#007300", height=40, width=25)
+    finish_button = CTkButton(button_frame,
+                              text="Complete Purchase",
+                              command=finalize_purchase,
+                              fg_color="#008000",
+                              hover_color="#007300",
+                              height=40,
+                              width=25)
     finish_button.pack(side="right", padx=(5, 0), pady=10)
 
     # Configure cart_frame to expand vertically
@@ -121,12 +204,5 @@ def frame_orders(frame):
     # Bind double click event to remove item from cart
     cart_listbox.bind("<Double-Button-1>", lambda event: remove_item(event))
 
-def remove_item(event):
-    global selected_products
-    index = cart_listbox.curselection()
-    if index:
-        item = cart_listbox.get(index)
-        product, _ = item.split(" x ")
-        if messagebox.askokcancel("Remove Item", f"Do you want to remove {product} from the cart?"):
-            del selected_products[product]
-            update_cart_display()
+# ------------------------------------------------------------------------------
+# End of script
