098c565ae2b50c7879eb175222aaaf4611630af2
FedeTejada <tejafede@gmail.com>
2024-05-27 14:08:20 -0300

    Change in main

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from src.models import Base, Product, Order, OrderProduct
 from src.config import engine
 from src.service.serviceProduct import addProducts
 from src.service.serviceProduct import *
-
+from src.service.change_products import frame_modif_product
 
 def createTables():
     Base.metadata.create_all(engine)
     
 def main():
+    frame_modif_product()
     #name = input("Add new name product:\n")
     #price = int(input("insert new price of the product:\n"))
     #addProducts(name,price)
-    updateProduct(2,"pizza",800)
     
     
 if __name__ == "__main__":
